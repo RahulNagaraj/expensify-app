6a00ddb510daa330af292ad2f6568377b6c531eb
Rahul N <rahul.n@ge.com>
2018-05-06 23:36:37 +0530

    expenses summary header

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilter from './ExpenseListFilter';
+import ExpensesSummary from './ExpensesSummary';
 
 const ExpenseDashboardPage = () => (
   <div>
     <p>This is add expense component</p>
+    <ExpensesSummary />
     <ExpenseListFilter />
     <ExpenseList />
   </div>
