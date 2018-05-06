4c449ff731d1fddfe7af940c8181369e37c21dc4
Rahul N <rahul.n@ge.com>
2018-05-06 16:10:43 +0530

    selected expenses test

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,11 +1,11 @@
 import moment from 'moment';
 
 // Get Visible expenses
-const getVisibleExpenses = (expenses, {text, sortBy, startDate, endDate}) => {
+const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
   return expenses.filter(expense => {
     const createdAtMoment = moment(expense.createdAt);
     const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true;
-    const endDateMatch  = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true;
+    const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true;
     const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
     return startDateMatch && endDateMatch && textMatch;
@@ -13,7 +13,7 @@ const getVisibleExpenses = (expenses, {text, sortBy, startDate, endDate}) => {
     if (sortBy === 'date') {
       return a.createdAt < b.createdAt ? 1 : -1;
     }
-    else {
+    else if (sortBy === 'amount') {
       return a.amount < b.amount ? 1 : -1;
     }
   });
