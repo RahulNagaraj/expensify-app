4bc2c342ce36cd3630ab54bf76b2d9d45e550756
Rahul N <rahul.n@ge.com>
2018-05-06 17:34:18 +0530

    remove default dispatch actions

--- src/app.js
+++ src/app.js
@@ -11,23 +11,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.subscribe(() => {
-  console.log(store.getState());
-  const state = store.getState();
-  const visibleExpense = visibleExpenses(state.expenses, state.filters);
-  console.log('visibleExpense: ', visibleExpense);
-});
-
-store.dispatch(addExpense({description: 'Gas bill', amount: 500, createdAt: 10000}));
-store.dispatch(addExpense({description: 'Rent bill', amount: 8000, createdAt: 1000}));
-store.dispatch(addExpense({description: 'Water bill', amount: 600, createdAt: 90000}));
-
-store.dispatch(setTextFilter('bill'));
-
-// setTimeout(() => {
-//   store.dispatch(setTextFilter('water'));
-// }, 3000);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
