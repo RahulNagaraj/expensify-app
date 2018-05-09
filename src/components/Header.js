e4c8094e8243887dec4dcc4db12ea6f1dd301a5d
Rahul N <rahul.n@ge.com>
2018-05-09 23:41:28 +0530

    basic googe authentication

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => (
   <div>
     <header>
       <h1>Expensify</h1>
-      <NavLink exact={true} to="/" activeClassName="is-active">Dashboard </NavLink>
+      <NavLink exact={true} to="/dashboard" activeClassName="is-active">Dashboard </NavLink>
       <NavLink to="/create" activeClassName="is-active">Create Expense </NavLink>
       <NavLink to="/help" activeClassName="is-active">Help </NavLink>
     </header>
